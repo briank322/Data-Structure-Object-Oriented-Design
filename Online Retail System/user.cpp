0547af5217dd57737c51804b76ea5e835001f607
Bone Seung (Brian) Koo <bsk32295@gmail.com>
2019-08-31 00:01:17 -0700

    Create user.cpp

---
+++ Online Retail System/user.cpp
@@ -0,0 +1,42 @@
+#include "user.h"
+using namespace std;
+
+User::User() : name_("unknown"), balance_(0.0), type_(1)
+{
+
+}
+User::User(std::string name, double balance, int type, unsigned long long hashcode) :
+    name_(name), balance_(balance), type_(type), hashcode_(hashcode)
+{
+
+}
+
+User::~User()
+{
+
+}
+
+
+std::string User::getName() const
+{
+    return name_;
+}
+
+double User::getBalance() const
+{
+    return balance_;
+}
+
+unsigned long long User::getHashcode() const
+{
+	return hashcode_;
+}
+void User::deductAmount(double amt)
+{
+    balance_ -= amt;
+}
+
+void User::dump(std::ostream& os)
+{
+    os << name_ << " "  << balance_ << " " << type_ <<" "<<hashcode_<< endl;
+}
